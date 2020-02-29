0c9de9b42b2c74ca352def09776dfcd4be0b90c1
Patrick Duquette <duquetteman@gmail.com>
2020-02-29 11:38:31 -0600

    Updated email link.

--- src/Components/About.js
+++ src/Components/About.js
@@ -35,7 +35,7 @@ class About extends Component {
 						         {city} {state}, {zip}
                    </span><br />
 						   <span>{phone}</span><br />
-                     <span>{email}</span>
+                     <span><a href="mailto:`${email}`">{email}</a></span>
 					   </p>
                </div>
                <div className="columns download">
