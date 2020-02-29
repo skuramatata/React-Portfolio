f4ebf4454cb4f1936c662df9fbad68a0f157a554
Patrick Duquette <duquetteman@gmail.com>
2020-02-29 11:29:24 -0600

    Updated favicon.ico file.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Patrick Duquette</li>
+              
            </ul>
 
         </div>
